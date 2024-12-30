5e84228b603549111a9742c18f33003bce72922f
rudialfa <rudih4236@gmail.com>
2024-12-30 18:29:09 +0700

    route semantic

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get('/', function () {
 Route::get('/html', function () {
     return view('program_web_dasar.html_dasar');
 });
+
+Route::get('/semantic', function () {
+    return view('program_web_dasar.semantic_html');
+});
