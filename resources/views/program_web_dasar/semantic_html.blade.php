f7de4946916fbcee445dec7e99750e043d17df58
rudialfa <rudih4236@gmail.com>
2024-12-30 18:34:27 +0700

    comand

--- resources/views/program_web_dasar/semantic_html.blade.php
+++ resources/views/program_web_dasar/semantic_html.blade.php
@@ -6,9 +6,14 @@
     <title>Semantic HTML</title>
 </head>
 <body>
+
+    {{-- header --}}
     <header>
         <h1>Logo Perusahaan</h1>
     </header>
+    {{-- batas header --}}
+    
+    {{-- navbar --}}
     <nav>
         <ul>
             <li><a href="#home">Beranda</a></li>
@@ -16,12 +21,20 @@
             <li><a href="#contact">Kontak</a></li>
         </ul>
     </nav>
+    {{-- batas navbar --}}
+    
+    {{-- main / body --}}
     <main>
         <h2>Selamat Datang di Website Kami</h2>
         <p>Ini adalah contoh layout dengan semantic HTML.</p>
     </main>
+    {{-- batas main / body --}}
+
+    {{-- footer --}}
     <footer>
         <p>&copy; 2024 Perusahaan XYZ. Semua Hak Dilindungi.</p>
     </footer>
+    {{-- batas footer --}}
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// html file
 Route::get('/html', function () {
     return view('program_web_dasar.html_dasar');
 });
 
+// semantic
 Route::get('/semantic', function () {
     return view('program_web_dasar.semantic_html');
 });
